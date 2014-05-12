47923576981d8856f78c1b971a537f3231a2365f
Douglas Christopher Wilson <doug@somethingdoug.com>
2014-05-12 18:14:16 -0400

    tests: remove use of connect

--- test/cookieParser.js
+++ test/cookieParser.js
@@ -1,41 +1,35 @@
-var connect = require('connect')
-  , request = require('supertest')
-  , signature = require('cookie-signature');
 
-var app = connect();
-
-app.use(connect.cookieParser('keyboard cat'));
-
-app.use(function(req, res, next){
-  if ('/signed' != req.url) return next();
-  res.end(JSON.stringify(req.signedCookies));
-});
-
-app.use(function(req, res, next){
-  res.end(JSON.stringify(req.cookies));
-});
+var cookieParser = require('..')
+var http = require('http')
+var request = require('supertest')
+var signature = require('cookie-signature')
 
 describe('connect.cookieParser()', function(){
+  var server
+  before(function(){
+    server = createServer('keyboard cat')
+  })
+
   describe('when no cookies are sent', function(){
     it('should default req.cookies to {}', function(done){
-      request(app)
+      request(server)
       .get('/')
-      .expect('{}', done);
+      .expect(200, '{}', done)
     })
 
     it('should default req.signedCookies to {}', function(done){
-      request(app)
+      request(server)
       .get('/signed')
-      .expect('{}', done);
+      .expect(200, '{}', done)
     })
   })
 
   describe('when cookies are sent', function(){
     it('should populate req.cookies', function(done){
-      request(app)
+      request(server)
       .get('/')
       .set('Cookie', 'foo=bar; bar=baz')
-      .expect('{"foo":"bar","bar":"baz"}', done);
+      .expect(200, '{"foo":"bar","bar":"baz"}', done)
     })
   })
 
@@ -44,29 +38,49 @@ describe('connect.cookieParser()', function(){
     // TODO: "bar" fails...
 
     it('should populate req.signedCookies', function(done){
-      request(app)
+      request(server)
       .get('/signed')
       .set('Cookie', 'foo=s:' + val)
-      .expect('{"foo":"foobarbaz"}', done);
+      .expect(200, '{"foo":"foobarbaz"}', done)
     })
 
     it('should remove the signed value from req.cookies', function(done){
-      request(app)
+      request(server)
       .get('/')
       .set('Cookie', 'foo=s:' + val)
-      .expect('{}', done);
+      .expect(200, '{}', done)
     })
 
     it('should omit invalid signatures', function(done){
-      request(app)
+      server.listen()
+      request(server)
       .get('/signed')
       .set('Cookie', 'foo=' + val + '3')
-      .expect('{}', function(){
-        request(app)
+      .expect(200, '{}', function(err){
+        if (err) return done(err)
+        request(server)
         .get('/')
         .set('Cookie', 'foo=' + val + '3')
-        .expect('{"foo":"foobarbaz.CP7AWaXDfAKIRfH49dQzKJx7sKzzSoPq7/AcBBRVwlI3"}', done);
+        .expect(200, '{"foo":"foobarbaz.CP7AWaXDfAKIRfH49dQzKJx7sKzzSoPq7/AcBBRVwlI3"}', done)
       });
     })
   })
 })
+
+function createServer(secret) {
+  var _parser = cookieParser(secret)
+  return http.createServer(function(req, res){
+    _parser(req, res, function(err){
+      if (err) {
+        res.statusCode = 500
+        res.end(err.message)
+        return
+      }
+
+      var cookies = '/signed' === req.url
+        ? req.signedCookies
+        : req.cookies
+      res.end(JSON.stringify(cookies))
+    })
+  })
+}
